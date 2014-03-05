6d53e5ae8a2457d253856734df4c183ac739af8d
alex <alex.tong007@gmail.com>
2014-03-05 10:37:33 +0800

    添加isArray注释说明

--- utils/base.js
+++ utils/base.js
@@ -10,7 +10,11 @@
 		isFunction: function(obj) {
 			return toString.call(obj) === "[object Function]";
 		},
-
+		//像我们所知道的，用 instanceof 和 constructor 是最直接的、简单的方式：
+		// var arr = [];
+		// arr instanceof Array; // true
+		// arr.constructor == Array; //true
+		// 只是，由于在不同 iframe 中创建的 Array 并不共享 prototype。如果这样用。麻烦就来了。那么，如果要应用在框架中，这种方式肯定是行不通的。
 		isArray: function(obj) {
 			return toString.call(obj) === "[object Array]";
 		}
